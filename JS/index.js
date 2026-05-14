6777b3b3eb50abaf4ebe85d090a6986131805e2e
daniel-caleb <cheruiyotdanielcaleb@gmail.com>
2026-05-14 17:24:24 +0300

    Divs-Classes Update + Intro to JS

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+let a = 10
+let b = 20
+b = 50
+console.log(a)
+console.log(b)
+console.log(a + b) 
+const c = "330"
+let routeName = "Kitengela"
+console.log(c)
+console.log(routeName)
+console.log(typeof routeName)
+console.log(typeof a)
+console.log(typeof c)
+let isRaining = true
+console.log(isRaining)
+console.log(typeof isRaining)
+let d = null
+console.log(d)
+console.log(typeof d)
+let e
+console.log(e)
+console.log(typeof e)
