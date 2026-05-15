a24f44a39ac69e09aee4fd9ff2114be5e71c3e99
daniel-caleb <cheruiyotdanielcaleb@gmail.com>
2026-05-15 11:11:31 +0300

    control flows

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,95 @@
-let a = 10
-let b = 20
-b = 50
-console.log(a)
-console.log(b)
-console.log(a + b) 
-const c = "330"
-let routeName = "Kitengela"
-console.log(c)
-console.log(routeName)
-console.log(typeof routeName)
-console.log(typeof a)
-console.log(typeof c)
-let isRaining = true
-console.log(isRaining)
-console.log(typeof isRaining)
-let d = null
-console.log(d)
-console.log(typeof d)
-let e
-console.log(e)
-console.log(typeof e)
+// let a = 10
+// let b = 20
+// b = 50
+// console.log(a)
+// console.log(b)
+// console.log(a + b) 
+// const c = "330"
+// let routeName = "Kitengela"
+// console.log(c)
+// console.log(routeName)
+// console.log(typeof routeName)
+// console.log(typeof a)
+// console.log(typeof c)
+// let isRaining = true
+// console.log(isRaining)
+// console.log(typeof isRaining)
+// let d = null
+// console.log(d)
+// console.log(typeof d)
+// let e
+// console.log(e)
+// console.log(typeof e)
+
+//Arithmetic Operators
+// let x = 10
+// let y = 5
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+// console.log(x % y)
+
+// Comparison Operators
+// let a = 10
+// let b = 20
+// console.log(a == b)
+// console.log(a != b)
+// console.log(a < b)
+// console.log(a > b)
+// console.log(a <= b)
+// console.log(a >= b)
+
+// Logical Operators
+// let isSunny = false
+// let isWarm = false
+// console.log(isSunny && isWarm)
+// console.log(isSunny || isWarm)
+// console.log(!isSunny)
+
+
+// String Concatenation
+// let firstName = "John"
+// let lastName = "Doe"
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
+
+// Control Structures
+// let age = 28
+// if (age >= 18) {
+//     console.log("You are an adult.")
+// } else {
+//     console.log("You are a minor.")
+// }
+
+// let examScore = 85
+// if (examScore >= 90  && examScore <= 100) {
+//     console.log("Grade: Distinction");
+// } else if (examScore >= 50 && examScore < 90) {
+//     console.log("Grade: Credit");
+// } else if (examScore >= 40 && examScore < 50) {
+//     console.log("Grade: Pass");
+// } else {
+//     console.log("Grade: Fail");
+// }
+
+let day = "Tuesday"
+switch (day) {
+    case "Monday":
+        console.log("It's Monday.");
+        break;
+    case "Tuesday":
+        console.log("It's Tuesday.");
+        break;
+    case "Wednesday":
+        console.log("It's Wednesday.");
+        break;
+    case "Thursday":
+        console.log("It's Thursday.");
+        break;
+    case "Friday":
+        console.log("It's Friday.");
+        break;
+    default:
+        console.log("It's not a weekday.");
+}
